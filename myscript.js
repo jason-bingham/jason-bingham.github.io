8bf283e33f1cd4a78576dcef348893c8769e9ea3
jbingham223 <jbingham223@gmail.com>
2022-12-28 20:33:34 -0300

    header responsiveness improvmenets

--- myscript.js
+++ myscript.js
@@ -1,28 +1,43 @@
 const contactButt = document.querySelector(".contact-button");
 const contactTab = document.querySelector(".contact-tab");
 const aboutButt = document.querySelector(".about-button");
-const aboutButtsvg = document.querySelector(".about-svg");
 const aboutTab = document.querySelector(".about-tab");
 const titleButt = document.querySelector(".title-button");
 const titleTab = document.querySelector(".title-tab");
+const aboutWrap = document.querySelector(".about-wrapper");
+const contactWrap = document.querySelector(".contact-wrapper");
+const headWrap = document.querySelector(".header-content-wrapper");
+const titleWrap = document.querySelector(".title-wrapper");
+
 
 contactButt.addEventListener("click", function (e) {
   if (contactTab.classList.contains("closed")) {
     contactTab.classList.remove("closed");
+    contactWrap.classList.remove("hidden");
+    headWrap.classList.add("hidden");
   } else {
     contactTab.classList.add("closed");
+    contactWrap.classList.add("hidden");
     aboutTab.classList.add("closed");
+    aboutWrap.classList.add("hidden");
     titleTab.classList.add("closed");
+    titleWrap.classList.add("hidden");
+    headWrap.classList.remove("hidden");
   }
 })
 
 aboutButt.addEventListener("click", function (e) {
   if (aboutTab.classList.contains("closed")) {
-  contactTab.classList.remove("closed");
-  aboutTab.classList.remove("closed");
+    aboutTab.classList.remove("closed");
+    aboutWrap.classList.remove("hidden");
+    contactTab.classList.remove("closed");
+    headWrap.classList.add("hidden");
   } else {
     aboutTab.classList.add("closed");
+    aboutWrap.classList.add("hidden");
     titleTab.classList.add("closed");
+    titleWrap.classList.add("hidden");
+    contactWrap.classList.remove("hidden");
   }
 });
 
@@ -31,8 +46,14 @@ titleButt.addEventListener("click", function (e) {
     contactTab.classList.remove("closed");
     aboutTab.classList.remove("closed");
     titleTab.classList.remove("closed");
+    titleWrap.classList.remove("hidden");
+    headWrap.classList.add("hidden");
+    contactWrap.classList.add("hidden");
+    aboutWrap.classList.add("hidden");
   } else {
     titleTab.classList.add("closed");
+    titleWrap.classList.add("hidden");
+    aboutWrap.classList.remove("hidden");
   }
 });
 
