f1da57801b68f8ec94c004954dd1aa95ee701ab5
jbingham223 <jbingham223@gmail.com>
2023-01-16 22:41:09 -0300

    Navbar, Favicons

--- script-hamburger.js
+++ script-hamburger.js
@@ -1,14 +1,16 @@
 // HAMBURGER
 
-const ham = document.getElementById("ham-main");
+const ham = document.querySelector(".ham-main");
+const menu = document.querySelector(".ham-menu");
 
 ham.addEventListener("click", () => {
-  console.log("called")
   const isOpen = ham.getAttribute("aria-expanded");
   if (isOpen === "false") {
     ham.setAttribute("aria-expanded", "true");
+    menu.setAttribute("aria-expanded", "true");
   } else {
     ham.setAttribute("aria-expanded", "false");
+    menu.setAttribute("aria-expanded", "false");
   }
   ham.classList.remove("preload");
 });
